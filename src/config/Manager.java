01f96db565a61fb7fda1582f5493939e549af093
mauro <mauro@mauro-PC>
2013-06-02 21:56:08 -0300

    pantalla grafica de bienvenida

---
+++ src/config/Manager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author mauro
+ * LA CLASE MANAGER ES LA QUE ADMINISTRA LA CARGA Y ACCESO A LOS DISTINTOS MODULOS
+ * LEE EL ARCHIVO DE CONFIGURACION Y SE CONECTA POR MEDIO DE ESOS PARAMETROS A LA BASE, TOMANDO EN CUENTA
+ * QUE DE ACUERDO A LOS PERMISSOS Y LLAVE CUALES SON LOS MODULOS A LOS QUE ACCEDE EL USUARIO
+ * 
+ */
+public class Manager {
+    
+}
